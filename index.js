8a500b661ae8b7619e37066205eadd2a533b20f5
Lukas <lukassh@gmail.com>
2019-03-08 18:24:14 +0100

    zadanie 17.6

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 var OSinfo = require('./modules/osinfo');
 var Convert = require('./modules/convertTime');
+var event = require('events');
+var EventEmitter = event.EventEmitter;
+// lub var EventEmitter = require('events').EventEmitter; - potrzebujemy tylko 1 klasy
+var emitter = new EventEmitter;
+
+emitter.on('berforeCommand', function(instruction) {
+    console.log('You wrote: ' + instruction + ' trying to run command.');
+});
+emitter.on('afterCommand', function() {
+    console.log('Finished command');
+});
 
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
     var input = process.stdin.read();
     if(input !== null) {
         var instruction = input.trim();
+        // berfore Command
+        emitter.emit('berforeCommand', instruction);
         switch(instruction) {
             case '/exit':
                 process.stdout.write('Quitting app!\n');
@@ -19,6 +32,8 @@ process.stdin.on('readable', function() {
             default:
                 process.stderr.write('Wrong instruction!\n Choose: /exit, / sayhello or /getOSinfo\n');
         };
+        // afterCommand
+        emitter.emit('afterCommand');
     }
 });
 
